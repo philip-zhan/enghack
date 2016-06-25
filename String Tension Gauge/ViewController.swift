da8732677288b202871069f181d64afad3e81acf
philiz <zhw584784058@gmail.com>
2016-06-25 16:53:54 -0400

    Initial Commit

---
+++ String Tension Gauge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  String Tension Gauge
+//
+//  Created by philip zhan on 2016-06-25.
+//  Copyright © 2016 P&C. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
